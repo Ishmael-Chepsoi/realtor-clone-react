7d96802790bf19b2957e8ec8c8084c628456c996
Ishmael-Chepsoi <ishmaelchepsoi@gmail.com>
2023-10-19 05:53:03 +0300

    Create Listing page ui

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Header from "./components/Header";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import PrivateRoute from "./components/PrivateRoute";
+import CreateListing from "./pages/CreateListing";
 function App() {
   return (
     <>
@@ -24,6 +25,7 @@ function App() {
      <Route path="/sign-in" element={<Signin />} />
      <Route path="/sign-up" element={<Signup />} />
      <Route path="/Forgot-password" element={<Forgotpassword />} />
+     <Route path="/create-listing" element={<CreateListing />} />
     </Routes>
    </Router>
    <ToastContainer
--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,10 +1,11 @@
 import { getAuth, updateProfile } from 'firebase/auth';
 import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import {db} from '../firebase'
 import { doc} from 'firebase/firestore';
-
+import {FcHome} from "react-icons/fc"
+// import { icons } from 'react-icons';
 export default function Profile() {
   const [changeDetails, setChangeDetails] = useState(false);
   const auth = getAuth();
@@ -80,6 +81,18 @@ export default function Profile() {
             cursor-pointer'>sign out</p>
           </div>
         </form>
+        <button type='submit' className='w-full bg-blue-600
+        text-white uppercase px-7 py-3 text-sm font-medium
+        rounded shadow-md hover:bg-blue-700 transition duration-150
+        ease-in-out hover:shadow-lg active:bg-blue-800'>
+          <Link to="/create-listing" className='flex
+          justify-center items-center'>
+            <FcHome className='mr-2 text-3xl bg-red-200
+            rounded-full p-1 border-2'/>
+            Sell or rent your home
+          </Link>
+
+        </button>
       </div>
     </section>
   )
